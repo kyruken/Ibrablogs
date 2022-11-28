13476c84ac00a3bbd34729eb3585f412e7ae6191
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-28 14:46:24 -0800

    erase comment prop

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 
 export default function Blog(props) {
-    const commentElements = props.comments.map(comment => {
-        <div>{comment}</div>
-    })
     return (
-        <div>
+        <div onClick={props.handleClick}>
             <h2>{props.title}</h2>
             <p>{props.body}</p>
             <p>{props.timestamp}</p>
-
-            <div>
-                {commentElements}
-            </div>
         </div>
     )
 }
