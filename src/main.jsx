5a8d1fefcd6d4cf24046fd98c38e0f4d1c05f998
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-29 17:09:42 -0800

    create router

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from 'react-router-dom';
+import App from './App';
+import ErrorPage from './error-page';
+import Blogpage from './Blogpage';
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/posts/:postId',
+    element: <Blogpage />
+  }
+
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <RouterProvider router={router} />
 )
