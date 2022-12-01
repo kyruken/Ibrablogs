d69fdca37c08d834ca212b32227215e1e27a2780
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-30 17:40:39 -0800

    add homepage tags and styles

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Blog(props) {
     return (
-        <div class="blog padding-all-1" onClick={props.handleClick}>
+        <div className="blog padding-all-1" onClick={props.handleClick}>
             <h2>{props.title}</h2>
             <p>{props.body}</p>
             <p>{props.timestamp}</p>
